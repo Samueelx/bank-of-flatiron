340b2afc38e4b2d0e8a065a368b02c16a4e9f3ce
Samueelx <gsammygitau@gmail.com>
2023-01-17 10:47:18 +0300

    Added and styled the form.

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Form() {
+    const handleChange = () => {
+
+    }
+
+    return (
+        <div className="m-8 p-6 bg-white">
+            <form className="mx-auto rounded">
+                <label htmlFor="description" className="block mx-auto"> Description
+                    <input type="text" onChange={handleChange} className="form-input block mx-auto px-4 py-3"/>
+                </label>
+
+                <label htmlFor="category" className="block mx-auto"> Category
+                    <input type="text" onChange={handleChange} className="form-input block mx-auto px-4 py-3"/>
+                </label>
+
+                <label htmlFor="amount" className="block mx-auto"> Amount
+                    <input type="text" onChange={handleChange} className="form-input block mx-auto px-4 py-3"/>
+                </label>
+
+                <button type="submit" className="block bg-transparent hover:bg-blue-800 text-blue-500 hover:text-white font-bold py-2 px-4 rounded border border-blue-600 hover:border-transparent mx-auto my-4">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
